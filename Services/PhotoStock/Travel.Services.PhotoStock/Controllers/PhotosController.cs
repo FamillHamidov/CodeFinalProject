a35f352aaa65370dacd2f72f0e5034ccf7acb1a5
FamillHamidov <famil.hemid@gmail.com>
2023-03-30 02:21:59 +0400

    Photo Stock Microservice Finally

---
+++ Services/PhotoStock/Travel.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travel.Services.PhotoStock.Dtos;
+using Travel.Shared.ControllerBases;
+using Travel.Shared.Dtos;
+
+namespace Travel.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                var returnPath = photo.FileName;
+                PhotoDto photoDto = new() { Url = returnPath };
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo not found", 400));
+        }
+        [HttpGet]
+        public IActionResult PhotoDelete(string url)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", url);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
