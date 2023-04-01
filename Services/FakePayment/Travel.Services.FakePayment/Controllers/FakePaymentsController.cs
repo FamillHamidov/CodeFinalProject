b85d68320a3804f238885a25c59f870859049304
FamillHamidov <famil.hemid@gmail.com>
2023-04-01 17:23:04 +0400

    FakePayment Start

---
+++ Services/FakePayment/Travel.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travel.Shared.ControllerBases;
+using Travel.Shared.Dtos;
+
+namespace Travel.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult Payment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
